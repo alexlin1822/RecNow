4cc04806b1f8913adf02561c8a8da971bb0a4555
Alex Lin <alexlin1822@hotmail.com>
2021-05-15 22:52:58 -0400

    strapi init

---
+++ back/recnow/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4ccd3567d672921ba40e0402a974015d'),
+    },
+  },
+});
---
+++ back/recnow/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8b6ca001-6733-4bd0-a5f0-6ca08d6cc5d6'
+};
